0f25c97da6bf799ee4988fbb88f52243bce21329
bishal1993 <maharjanbishal19@gmail.com>
2024-02-28 17:06:22 +0100

    generic dao is added and other dao interfaces is deleted

---
+++ src/main/java/com/seatrade/dao/CargoDaoImplementation.java
@@ -0,0 +1,36 @@
+package com.seatrade.dao.daoimplement;
+
+import com.seatrade.dao.CargoDao;
+import com.seatrade.dao.GenericDAO;
+import com.seatrade.entity.Cargo;
+
+import java.util.List;
+
+public class CargoDaoImplementation implements GenericDAO<Cargo> {
+
+
+    @Override
+    public Cargo create(Cargo cargo) {
+        return null;
+    }
+
+    @Override
+    public Cargo update(Cargo cargo) {
+        return null;
+    }
+
+    @Override
+    public Cargo get(Cargo cargo) {
+        return null;
+    }
+
+    @Override
+    public void delete(Cargo cargo) {
+
+    }
+
+    @Override
+    public List<Cargo> listAll() {
+        return null;
+    }
+}
---
+++ src/main/java/com/seatrade/dao/CompanyAppDaoImplementation.java
@@ -0,0 +1,35 @@
+package com.seatrade.dao.daoimplement;
+
+import com.seatrade.dao.GenericDAO;
+import com.seatrade.entity.CompanyApp;
+import com.seatrade.dao.CompanyAppDao;
+
+import java.util.List;
+
+public class CompanyAppDaoImplementation implements GenericDAO<CompanyApp> {
+
+    @Override
+    public CompanyApp create(CompanyApp companyApp) {
+        return null;
+    }
+
+    @Override
+    public CompanyApp update(CompanyApp companyApp) {
+        return null;
+    }
+
+    @Override
+    public CompanyApp get(CompanyApp companyApp) {
+        return null;
+    }
+
+    @Override
+    public void delete(CompanyApp companyApp) {
+
+    }
+
+    @Override
+    public List<CompanyApp> listAll() {
+        return null;
+    }
+}
